407c45061322b5062ee74a1f427a81c1e4e96e28
codeandkey <jtst@iastate.edu>
2020-10-29 01:33:05 -0500

    bitboard: add tests

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -20,10 +20,26 @@ pub fn to_pretty(b: u64) -> String {
     output
 }
 
-pub fn for_each<F>(mut b: u64, mut c: F) where F: FnMut(Square) {
+pub fn for_each<F>(mut b: u64, mut c: F)
+where
+    F: FnMut(Square),
+{
     while b != 0u64 {
         let s: Square = Square::from_index(b.trailing_zeros() as usize).unwrap();
         b ^= s.mask();
         c(s);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn bitboard_to_pretty_works() {
+        assert_eq!(
+            to_pretty(0xfe79e8a5c9b8095c),
+            ".#######\n#..####.\n...#.###\n#.#..#.#\n#..#..##\n...###.#\n#..#....\n..###.#."
+        );
     }
 }
